05ee0adba549d877b98af63333439013a1f9c6b8
Philippe Pepiot <phil@lowatt.fr>
2021-08-23 11:21:21 +0200

    Fix PEP440 compatible versions
    During upload to pypi and testpypi, versions generated by setuptools_scm must be pep440 compatible.

--- setup.py
+++ setup.py
@@ -20,5 +20,18 @@
 # THE SOFTWARE.
 from setuptools import setup
 
+
+def local_scheme(version: str) -> str:
+    """Generate a PEP440 compatible version if PEP440_VERSION is enabled"""
+    import os
+    import setuptools_scm.version  # only present during setup time
+
+    if "PEP440_VERSION" in os.environ:
+        return ""
+    version = setuptools_scm.version.get_local_node_and_date(version)
+    assert isinstance(version, str), version
+    return version
+
+
 if __name__ == "__main__":
-    setup(use_scm_version=True)
+    setup(use_scm_version={"local_scheme": local_scheme})
